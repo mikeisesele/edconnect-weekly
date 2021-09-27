d30f98f73d031378cacda6d8372b816a83fb7997
CodaarX <micahelisesele@gmail.com>
2021-09-27 06:53:07 +0100

    change project creator

--- views/Project.jsx
+++ views/Project.jsx
@@ -14,7 +14,7 @@ import "./styles/style.css";
 const ProjectLayout = ({ response }) => {
   console.log(response)
   const project  = response?.data?.project ? response.data.project : null;
-  const projectCreator = response?.currentUser ? response.currentUser : "";    
+  const projectCreator = response?.data?.projectCreator ? response.data.projectCreator : {};    
   const projectId = project?._id ? project._id.toString() : "";
   const stringProjectCreatorId = projectCreator?._id? projectCreator._id.toString() : "";
   const stringUserId = response?.currentUser?._id ?  response.current.user._id.toString() : "";
