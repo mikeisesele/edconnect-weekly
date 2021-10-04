16eefc20e27ba4694e545ef761bb03616124f026
CodaarX <micahelisesele@gmail.com>
2021-10-04 17:53:57 +0100

    clean port

--- server/index.js
+++ server/index.js
@@ -28,16 +28,6 @@ const store = new MongoDBStore({
   collection: "sessions",
 });
 
-  /**
- * @Desc listen to post when mongo connection is successful
- * @param {number} port - port number to listen to
- * @param {function} callback - callback function to be called when server is started
- * @returns {void}
- */
-  app.listen(SERVER_PORT, () => {
-    console.log("Edconnect server is live. listening on port " + SERVER_PORT);
-    console.log("connecting to database...");
-  });
 
 /**
  * @Desc
@@ -75,8 +65,6 @@ register(app).then(() => {
     })
   );
 
-
-  app.use(cors())
 
   /**
    * @Desc set global variable in express
@@ -117,5 +105,16 @@ register(app).then(() => {
    * @Desc set up static files handling
    */
   app.use(express.static("public"));
+
+  /**
+   * @Desc listen to post when mongo connection is successful
+   * @param {number} port - port number to listen to
+   * @param {function} callback - callback function to be called when server is started
+   * @returns {void}
+   */
+  app.listen(SERVER_PORT, () => {
+    console.log("Edconnect server is live. listening on port " + SERVER_PORT);
+    console.log("connecting to database...");
+  });
 });
 
