7c33036825786f8823890d691e05544953855a44
CodaarX <micahelisesele@gmail.com>
2021-10-07 22:51:32 +0100

    refactored result var

--- views/validEmail.js
+++ views/validEmail.js
@@ -2,9 +2,9 @@
  * @desc Validates email address using regex
  */
 const isValidEmail = (email) => {
-  const re =
+  const result =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  return result.test(String(email).toLowerCase());
 };
 
 
