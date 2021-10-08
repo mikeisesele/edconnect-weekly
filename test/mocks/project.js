4e8f359342e5945590e5ac4a4254e41eb6028fcd
CodaarX <micahelisesele@gmail.com>
2021-10-08 01:57:28 +0100

    add test

---
+++ test/mocks/project.js
@@ -0,0 +1,78 @@
+
+// create an array of 10 objects with the above properties
+const projects = [
+    {
+        name: "Project 1",
+        description: "This is the first project",
+        tag: "tag1",
+        abstract: "This is the first project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 2",
+        description: "This is the second project",
+        tag: "tag2",
+        abstract: "This is the second project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 3",
+        description: "This is the third project",
+        tag: "tag3",
+        abstract: "This is the third project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 4",
+        description: "This is the fourth project",
+        tag: "tag4",
+        abstract: "This is the fourth project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 5",
+        description: "This is the fifth project",
+        tag: "tag5",
+        abstract: "This is the fifth project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+    
+        name: "Project 6",
+        description: "This is the sixth project",
+        tag: "tag6",
+        abstract: "This is the sixth project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 7",
+        description: "This is the seventh project",
+        tag: "tag7",
+        abstract: "This is the seventh project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 8",
+        description: "This is the eighth project",
+        tag: "tag8",
+        abstract: "This is the eighth project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 9",
+        description: "This is the ninth project",
+        tag: "tag9",
+        abstract: "This is the ninth project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+    {
+        name: "Project 10",
+        description: "This is the tenth project",
+        tag: "tag10",
+        abstract: "This is the tenth project",
+        createdBy: "614b19723f43a1fd47956f63",
+    },
+];
+
+
+module.exports = projects;
