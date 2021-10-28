cd8d9e01b224cd669ec7a3af2c0e45afe6f00524
CodaarX <micahelisesele@gmail.com>
2021-10-28 12:26:51 +0100

    refactored requirements6

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -6,7 +6,6 @@ const isLoggedIn = require("../middlewares/auth");
 const path = require('path')
 const userInSession = require("../../utils/userInSession");
 const render = require("../../utils/renderView");
-// import {render} from "../../utils/renderView";
 
 /**
  * @desc homepage route
--- utils/renderVIew.js
+++ utils/renderVIew.js
@@ -4,6 +4,3 @@
 module.exports = function render (res, page, message) {
   res.render(page, message);
 };
-
-// export default render;
-// module.exports = render
